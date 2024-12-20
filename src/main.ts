64d4bf05e461b14b60baf43cb07a42ee433a6903
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-12-20 23:20:50 +0700

    WIP 8

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as process from 'process';
-import { hostname } from 'os';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
@@ -21,11 +19,9 @@ async function bootstrap() {
       },
     }),
   );
+console.log("app is listen on port :" + process.env.PORT);
 
-  const port = process.env.PORT || 4000;
-  console.log(`app is listening on port: ${port}`);
+  await app.listen(8000);
 
-  await app.listen(port, '0.0.0.0');
 }
-
 bootstrap();

de43550795fa08e310339d2a6eafd3dd9321c5bf
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-12-21 09:12:52 +0700

    test 5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello-world')
+  @Get('helloWorld')
   getHello(): string {
     return this.appService.getHello();
   }

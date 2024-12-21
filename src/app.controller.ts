5cf396ab6c07817a9875db69a4861ec42e9c26ee
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-12-21 10:00:04 +0700

    FIXED

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/mongoose-status')
+  getMongooseStatus(): Promise<string> {
+    return this.appService.checkMongooseStatus();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import  mongoose from 'mongoose';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  async checkMongooseStatus() : Promise<string> {
+    const state = {
+      0: 'Disconnected',
+      1: 'Connected',
+      2: 'Connecting',
+      3: 'Disconnecting',
+    }
+
+    const connectionState = mongoose.connection.readyState;
+    return state[connectionState]||"Unknown";
   }
 }

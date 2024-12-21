f86f32e172c748b2c190305fa6be8ee713225ba6
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-12-21 08:40:49 +0700

    cleanup

--- src/express.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from 'express';
-
-declare module 'express' {
-  export interface Request {
-    user?: any; // Bạn có thể thay 'any' bằng một kiểu cụ thể nếu bạn biết kiểu của `user`
-  }
-}

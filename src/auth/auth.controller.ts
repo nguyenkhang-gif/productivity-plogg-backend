ff3ac5fd1c6a1e1fedf3e59dfb245440a4b8df45
NGUYỄN NGUYÊN KHANG <67676210+nguyenkhang-gif@users.noreply.github.com>
2024-12-27 15:52:30 +0700

    WIP 1

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,8 +15,9 @@ export class AuthController {
       const { access_token, refresh_token, user } = respon;
       res.cookie('refresh_token', refresh_token, {
         httpOnly: true,
-        secure: false, // Chỉ gửi qua HTTPS
+        secure: true, // Chỉ gửi qua HTTPS
         sameSite: 'None', // Hoặc 'lax' nếu cần
+        
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
       });
       return res.status(HttpStatus.OK).json({
